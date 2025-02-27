69b8387362778be328e7fca8ad3401e98ebc35da
sjg-2903 <shubh.briotechno@gmail.com>
2025-02-27 17:03:43 +0530

    small cursor

--- src/components/Map.js
+++ src/components/Map.js
@@ -80,7 +80,7 @@ export default function Map() {
         mapTypeControl: false,
         mapTypeId: 'hybrid',
         tilt: 0,
-        draggableCursor: 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'24\' height=\'24\' viewBox=\'0 0 24 24\'><circle cx=\'12\' cy=\'12\' r=\'6\' fill=\'%2300FFFF\' stroke=\'%23000000\' stroke-width=\'2\'/></svg>") 12 12, auto',
+        draggableCursor: 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'24\' height=\'24\' viewBox=\'0 0 24 24\'><circle cx=\'12\' cy=\'12\' r=\'4\' fill=\'%2300FFFF\' stroke=\'%23000000\' stroke-width=\'2\'/></svg>") 12 12, auto',
     }), []);
 
     const onLoad = useCallback((map) => (mapRef.current = map), []);
